8296cb1e6f78261502c90d4c1b21a2d8d2680d73
killer23d <killer23d@yahoo.ca>
2025-11-25 10:41:55 -0800

    Update secrets.sh

--- lib/secrets.sh
+++ lib/secrets.sh
@@ -199,12 +199,14 @@ generate_argon2_hash() {
     
     case "$method" in
         python)
-            hash=$(python3 -c "
+            hash=$(printf '%s' "$password" | python3 -c "
+import sys
 from argon2 import PasswordHasher
 from argon2 import Type
 ph = PasswordHasher(time_cost=3, memory_cost=65536, parallelism=4, hash_len=32, salt_len=16, type=Type.ID)
-print(ph.hash('$password'))
-" 2>/dev/null)
+password = sys.stdin.read()
+print(ph.hash(password))
+")
             ;;
         cli)
             local salt
